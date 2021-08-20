84c26a15770964fe60e947448c06444161de20f0
shiv kumar <shivk7466@gmail.com>
2021-08-21 00:31:40 +0530

    Sendgrid email template ejs, attachments, test case, ci fixed

--- test/index.js
+++ test/index.js
@@ -6,7 +6,9 @@ const mocha = require("mocha");
 const should = chai.should();
 const expect = chai.expect;
 
-var multiMailer = require('..');
+var multiMailer = require('../lib/index');
+
+var resolver = require("../lib/resolver");
 
 
 describe('node-multi-mailer' , async () => {
@@ -21,39 +23,29 @@ describe('node-multi-mailer' , async () => {
         catch(err) { throw err};
     });
 
-    it('sendEmail should be a function', async () => {
-
-        try {
-
-            expect(multiMailer.sendEmail).to.be.a('function');
-        }
-        catch(err) { throw err};
-    });
-
-    it('sendHtmlEmail should be a function', async () => {
-
-        try {
-
-
-            expect(multiMailer.sendHtmlEmail).to.be.a('function');
-
-        }
-        catch(err) { throw err};
-    });
-    it('sendTextEmail should be a function', async () => {
-
-        try {
-            expect(multiMailer.sendTextEmail).to.be.a('function');
-
-        }
-        catch(err) { throw err};
-    });
-
     it('configuration should be a function', async () => {
 
         try {
-            expect(multiMailer.configuration).to.be.a('function');
 
+            expect(multiMailer.configuration).to.be.a('function');
+        }
+        catch(err) { throw err};
+    });
+
+    it('SendGrid should be a function', async () => {
+
+        try {
+
+            expect(multiMailer.SendGrid).to.be.an('object');
+        }
+        catch(err) { throw err};
+    });
+
+    it('sendTextEmail should be a function', async () => {
+
+        try {
+
+            expect(multiMailer.SendGrid.sendTextEmail).to.be.a('function');
         }
         catch(err) { throw err};
     });
@@ -61,10 +53,66 @@ describe('node-multi-mailer' , async () => {
     it('sendEjsTemplateWithData should be a function', async () => {
 
         try {
-            expect(multiMailer.sendEjsTemplateWithData).to.be.a('function');
 
+            expect(multiMailer.SendGrid.sendEjsTemplateWithData).to.be.a('function');
         }
         catch(err) { throw err};
     });
-    
+
+    it('ejsTemplate should be a function', async () => {
+
+        try {
+
+            expect(resolver.ejsTemplate).to.be.a('function');
+        }
+        catch(err) { throw err};
+    });
+
+    it('getAttachments should be a function', async () => {
+
+        try {
+
+            expect(resolver.getAttachments).to.be.a('function');
+        }
+        catch(err) { throw err};
+    });
+
+
+    it('getContentType should be a function', async () => {
+
+        try {
+
+            expect(resolver.getContentType).to.be.a('function');
+        }
+        catch(err) { throw err};
+    });
+
+    it('getFilePath should be a function', async () => {
+
+        try {
+
+            expect(resolver.getFilePath).to.be.a('function');
+        }
+        catch(err) { throw err};
+    });
+
+    it('isFolderExists should be a function', async () => {
+
+        try {
+
+            expect(resolver.isFolderExists).to.be.a('function');
+        }
+        catch(err) { throw err};
+    });
+
+
+    it('setMessage should be a function', async () => {
+
+        try {
+
+            expect(resolver.setMessage).to.be.a('function');
+        }
+        catch(err) { throw err};
+    });
+
 });
